0543f7e85e0ccdf239a008312930b6fc5ba0a0d7
Luzmila Rodriguez <luzmilayamilerodriguez@gmail.com>
2022-07-10 22:06:29 -0300

    Se implementó el componente card en Home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import MediaCard from '../components/card';
+
 export default function Home() {
   return (
     <div>
         <h1>
           Hello World
         </h1>
+        <MediaCard/>
     </div>
   )
 }
